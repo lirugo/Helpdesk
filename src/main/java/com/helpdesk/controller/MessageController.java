2b40aecd795744a0b813261e7c8bc64848735dc4
lirugo <roostik21@gmail.com>
2019-02-01 16:57:43 +0200

    Initial commit

---
+++ src/main/java/com/helpdesk/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.helpdesk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("messages")
+public class MessageController {
+
+    @GetMapping
+    public String list(){
+        return "helpdesk";
+    }
+
+}
