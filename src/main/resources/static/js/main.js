cbf05ef49c5df173923da6605a99bc600b56e3f6
lirugo <roostik21@gmail.com>
2019-02-04 13:42:34 +0200

    Divided to components

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,38 +2,13 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue({
     el: '#app',
     render: a => a(App)
 })
-
-
-// var messageAPI = Vue.resource('/messages{/id}')
-//
-// Vue.component('message-form', {
-//     props: ['messages'],
-//     data: function(){
-//         return {
-//             text: ''
-//         }
-//     },
-//     template:
-//         '<div>' +
-//             '<input type="text" placeholder="Write msg" v-model="text"/>' +
-//             '<input type="button" @click="save" value="Send"/>' +
-//         '</div>',
-//     methods: {
-//         save: function(){
-//             var message = { text: this.text }
-//
-//             messageAPI.save({}, message).then(result =>
-//                 result.json().then(data => {
-//                     this.messages.push(data)
-//                     this.text = ''
-//                 }))
-//         }
-//     }
-// })
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <messages-list :messages="messages"></messages-list>
-    </div>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title>Help Desk</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid>
+                <messages-list :messages="messages"></messages-list>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
